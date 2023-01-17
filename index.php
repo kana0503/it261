a300dea7bd1fea2e44feca0cac3c7ab5e833be57
kana0503 <116143517+kana0503@users.noreply.github.com>
2023-01-17 08:38:06 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+    content="width=device-width,initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible"
+    content="ie=edge">
+    <title>MyPortal page</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<h1><a href="index.php">Kanako's Portal Page</a></h1> 
+<h2> The navigation below represents our BIG Assigments.</h2>
+<div id="wrapeer">
+
+
+
+</div>
+<nav>
+    <ul>
+        <li><a href="">Switch</a></li>
+        <li><a href="">Troubleshoot</a></li>
+        <li><a href="">Calculator</a></li>
+        <li><a href="">Email</a></li>
+        <li><a href="">Database</a></li>
+        <li><a href="">Garelly</a></li>
+    </ul>
+</nav>
+<main>
+<h2>About Kanako</h2>
+<p> A Paragraph about yourself, i.e.</p>
+<img src="images/prfile.jpg" alt="Kanako">
+</main>
+
+
+</body>
+</html>

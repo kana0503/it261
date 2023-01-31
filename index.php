86c5c6f63e35d89b69ff2b310762916c368b19e3
kana0503 <116143517+kana0503@users.noreply.github.com>
2023-01-31 02:19:28 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -59,6 +59,16 @@
     <li><a href="weeks/week3/switch.php">switch.php</a></li>
     <li><a href="weeks/week3/index.php">index.php</a></li>
 </ol>
+
+<h3>Week 4</h3>
+<ol>
+    <li><a href="weeks/week4/form-get.php">form-get.php</a></li>
+    <li><a href="weeks/week4/form1.php">form1.php</a></li>
+    <li><a href="weeks/week4/form2.php">form2.php</a></li>
+    <li><a href="weeks/week4/form3.php">form3.php</a></li>
+    <li><a href="weeks/week4/form-arithmetic.php">form-arithmetic.php</a></li>
+    <li><a href="weeks/week4/celcius.php">celcius.php</a></li>
+</ol>
 </aside>
 
 
